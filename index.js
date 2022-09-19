5502969ba58162197273eba2f46ee13a94c61590
mifouts <fouts.mitch15@gmail.com>
2022-09-19 14:16:11 -0400

    starting up JS portion of project

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 //https://www.omdbapi.com/?apikey=3c851f46&s=fast
 
 
+const moviesContainerEl = document.querySelector('.movies__container');
+const landingContainerEl = document.querySelector('.landing__container');
+
+
+async function searchBarForm (event) {
+    const formValue = event.target.value;
+    const movie = await fetch(`https://www.omdbapi.com/?apikey=3c851f46&s=${formValue}`);
