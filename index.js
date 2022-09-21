92d5fe4ab09507fcc4b7dc251974035b91b829a9
mifouts <fouts.mitch15@gmail.com>
2022-09-21 15:34:01 -0400

    fixed js error now creating go back button

--- index.js
+++ index.js
@@ -20,7 +20,9 @@ async function searchBarForm(event) {
         moviesContainerEl.style.display = "flex";
         landingContainerEl.style.display = "none";
     }
-    moviesContainerEl.innerHTML = movieData.Search.slice(0, 6).map((movie) => moviesHTML(movie)).join("");
+    moviesContainerEl.innerHTML = movieData.Search.slice(0, 6)
+    .map((movie) => moviesHTML(movie))
+    .join("");
 
     event.preventDefault();
 }
